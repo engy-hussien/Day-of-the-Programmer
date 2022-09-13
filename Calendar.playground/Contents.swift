a2cef01c52b9fcf7ef542393e0bf5f88ff275037
Engy Hussien <engy.hussein@itworx.com>
2022-09-14 01:54:40 +0200

    Adding files
    Adding Files

---
+++ Calendar.playground/Contents.swift
@@ -0,0 +1,33 @@
+import UIKit
+import Foundation
+
+var greeting = "Hello, playground"
+
+func getPD(year: Int)-> String {
+    
+    while year >= 1700 && year <= 2700 {
+        if year >= 1700 && year < 1918 {
+            if year % 4 == 0 {
+                return "12.09.\(year)"
+            } else {
+                return "13.09.\(year)"
+            }
+            
+        } else if year > 1918 && year <= 2700 {
+            if (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0) {
+                return "12.09.\(year)"
+            } else {
+                return "13.09.\(year)"
+            }
+            
+        } else {
+            return "26.09.1918"
+        }
+    }
+    return "Unaviable year"
+}
+
+let result = getPD(year: 2717)
+print(result)
+
+
